de0bc88f2aa900872dc4d0dc50f84c12f54608c5
glicerico <suarezandres@gmail.com>
2020-03-02 16:40:39 +0800

    Rule swapper, sentence generation working

--- src/GrammarInducer.py
+++ src/GrammarInducer.py
@@ -5,23 +5,31 @@ import random
 
 REPOS = "/home/andres/repositories/"
 sys.path.insert(0, REPOS)
-from src.sentence_generator import GrammarSampler
+from src.sentence_generator import GrammarSampler, Grammar
 
 
 class GrammarInducer:
     """
     Induces a grammar by leveraging transformers
     """
+
     def __init__(self, grammar_file):
-        self.sampler = GrammarSampler(grammar_file)
-        self.mod_grammar = {}  # Local grammar, to modify it
-        self.reset_grammar()
+        self.orig_grammar = Grammar(grammar_file)
+        self.orig_sampler = GrammarSampler(self.orig_grammar)
+        self.mod_grammar = Grammar(grammar_file)  # Local grammar for testing new rules
+        self.mod_sampler = None
+
+    def init_mod_sampler(self):
+        """
+        Generate instance of GrammarSampler using the modified grammar
+        """
+        self.mod_sampler = GrammarSampler(self.mod_grammar)
 
     def reset_grammar(self):
         """
         Reset grammar to the one from GrammarSampler instance
         """
-        self.mod_grammar = copy.deepcopy(self.sampler.disj_dict)
+        self.mod_sampler = GrammarSampler(self.orig_grammar)
 
     def choose_random_rule(self, chosen_class=None):
         if chosen_class is None:
@@ -32,12 +40,15 @@ class GrammarInducer:
         return chosen_class, rule
 
     def choose_specific_rule(self, this_class, this_rule):
-        return this_class, self.sampler.disj_dict[this_class][this_rule]
+        return this_class, self.orig_sampler.disj_dict[this_class][this_rule]
 
-    def generate_sentences(self, num_sents=10, node=None, rule=None):
-        sents = [self.sampler.generate_parse(starting_node=node, starting_rule=rule)[0] for i in range(num_sents)]
+    @staticmethod
+    def generate_sentences(sampler, num_sents=10, node=None, rule=None, verbose=False):
+        sents = [sampler.generate_parse(starting_node=node, starting_rule=rule)[0] for _ in range(num_sents)]
         sents = list(set(sents))
-        print(f"Generated {len(sents)} unique sentences.")
+        if verbose:
+            print(f"Generated {len(sents)} unique sentences.")
+        return sents
 
     @staticmethod
     def swap_rule(original_rule):
@@ -49,16 +60,16 @@ class GrammarInducer:
         new_rule = [(y, x) for x, y in original_rule]  # Inverts all connectors in rule (and their word order)
         return new_rule
 
-    def modify_grammar(self, root_category, old_rule):
+    def swap_grammar(self, root_category, old_rule, verbose=False):
         connectors = {}
         new_rule = self.swap_rule(old_rule)
         # Replace old rule in current category
-        new_disjunct = tuple([new_rule if old_rule == x else x for x in self.sampler.disj_dict[root_category]])
-        self.mod_grammar[root_category] = new_disjunct
+        new_disjunct = tuple([new_rule if old_rule == x else x for x in self.orig_sampler.disj_dict[root_category]])
+        self.mod_grammar.disj_dict[root_category] = new_disjunct
 
         # Check which of the involved connectors are included in other rules in root_category
         for conn in old_rule:  # Assumes all connectors in new rule were changed
-            checks = [True if conn in rule else False for rule in self.mod_grammar[root_category]]
+            checks = [True if conn in rule else False for rule in self.mod_grammar.disj_dict[root_category]]
             connectors[conn] = any(checks)
 
         # For each connector in old rule, change its linked category rules. Assuming all connectors were swapped
@@ -66,7 +77,7 @@ class GrammarInducer:
             linked_class = list(conn)
             linked_class.remove(root_category)
             linked_disj = []
-            for rule in self.sampler.disj_dict[linked_class[0]]:
+            for rule in self.orig_sampler.disj_dict[linked_class[0]]:
                 this_rule = rule[:]  # Copy values
                 if conn in this_rule:
                     if duplicate:  # If connector of interest is still in root_category, keep rules connecting to them
@@ -78,41 +89,49 @@ class GrammarInducer:
                     linked_disj.append(this_rule[:])
                 else:
                     linked_disj.append(this_rule)
-            self.mod_grammar[linked_class[0]] = linked_disj  # Substitutes original rule
-        print(f"Original grammar {self.sampler.disj_dict}")
-        print(f"Modified grammar {self.mod_grammar}")
+            self.mod_grammar.disj_dict[linked_class[0]] = tuple(linked_disj)  # Substitutes original rule
+        if verbose:
+            print(f"Original grammar {self.orig_sampler.disj_dict}")
+            print(f"Modified grammar {self.mod_grammar.disj_dict}")
 
-def test_modify_grammar():
+        return new_rule
+
+
+def test_swap_grammar():
+    """
+    Tests if the method swapping the grammar works properly, for one example.
+    """
     test_inducer = GrammarInducer('../grammars/gram1.grammar')
 
-    print(test_inducer.sampler.disj_dict)
     test_class, test_rule = test_inducer.choose_specific_rule(1, 1)
-    inducer.modify_grammar(test_class, test_rule)
+    test_inducer.swap_grammar(test_class, test_rule, verbose=False)
 
-    expected_modified_grammar = {
-        0: [[(0, 1)], [(1, 0)], [(5, 0), (0, 1)], [(5, 0), (1, 0)], [(3, 0), (5, 0), (0, 1)], [(3, 0), (5, 0), (1, 0)]],
+    expected_disj_dict = {
+        0: ([(0, 1)], [(1, 0)], [(5, 0), (0, 1)], [(5, 0), (1, 0)], [(3, 0), (5, 0), (0, 1)], [(3, 0), (5, 0), (1, 0)]),
         1: ([(0, 1)], [(1, 0), (2, 1)], [(0, 1), (1, 4)], [(0, 1), (1, 2), (1, 4)]),
-        2: [[(1, 2)], [(2, 1)], [(5, 2), (1, 2)], [(5, 2), (2, 1)], [(3, 2), (5, 2), (1, 2)], [(3, 2), (5, 2), (2, 1)]],
+        2: ([(1, 2)], [(2, 1)], [(5, 2), (1, 2)], [(5, 2), (2, 1)], [(3, 2), (5, 2), (1, 2)], [(3, 2), (5, 2), (2, 1)]),
         3: ([(3, 2)], [(3, 0)]),
         4: ([(1, 4)],),
         5: ([(5, 0)], [(5, 2)])}
-    assert test_inducer.mod_grammar == expected_modified_grammar
+    assert test_inducer.mod_grammar.disj_dict == expected_disj_dict
+    print("Swapped grammar test passed!")
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Grammar Induction using transformers')
     parser.add_argument('--grammar', type=str, required=True, help='Grammar file to use')
+    parser.add_argument('--verbose', action='store_true', help='Print processing info?')
 
     args = parser.parse_args()
 
     inducer = GrammarInducer(args.grammar)
 
-    print(inducer.sampler.disj_dict)
-    rand_class, rand_rule = inducer.choose_random_rule()
-    # rand_class, rand_rule = inducer.choose_specific_rule(1, 1)
+    # rand_class, rand_rule = inducer.choose_random_rule()
+    rand_class, rand_rule = inducer.choose_specific_rule(1, 1)  # For testing purposes
+    inducer.generate_sentences(inducer.orig_sampler, node=rand_class, rule=rand_rule, num_sents=2, verbose=args.verbose)
 
-    inducer.modify_grammar(rand_class, rand_rule)
+    swapped_rule = inducer.swap_grammar(rand_class, rand_rule, verbose=args.verbose)
+    inducer.init_mod_sampler()
+    inducer.generate_sentences(inducer.mod_sampler, node=rand_class, rule=swapped_rule, num_sents=2, verbose=args.verbose)
 
-    # inducer.generate_sentences(node=rand_class, rule=rand_rule, num_sents=5)
-
-    test_modify_grammar()
-
+    test_swap_grammar()
